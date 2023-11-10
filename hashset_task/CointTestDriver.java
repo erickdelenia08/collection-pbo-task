52edf3caa47054a756416648578b2fca4e14b7f3
Erick Delenia <erickdelenia08@gmail.com>
2023-11-10 07:56:09 +0700

    change

--- hashset_task/CointTestDriver.java
+++ hashset_task/CointTestDriver.java
@@ -7,24 +7,25 @@ public class CointTestDriver {
         HashSet<Coin> bagOfCoins = new HashSet<Coin>();
         Coin coin1 = new Coin(1);
         Coin coin2 = new Coin(2);
-        Coin coin3 = new Coin(5);
-        Coin coin4 = new Coin(10);
-        Coin coin5 = new Coin(20);
-        Coin coin6 = new Coin(50);
-        Coin coin7 = new Coin(100);
-
-        // addition
+        Coin coin5 = new Coin(5);
         Coin coin10 = new Coin(10);
+        Coin coin20 = new Coin(20);
+        Coin coin50 = new Coin(50);
         Coin coin100 = new Coin(100);
-
+        
+        // addition
+//        Coin coin10 = new Coin(10);
+//        Coin coin100 = new Coin(100);
+        
         bagOfCoins.add(coin1);
         bagOfCoins.add(coin2);
-        bagOfCoins.add(coin3);
-        bagOfCoins.add(coin4);
         bagOfCoins.add(coin5);
-        bagOfCoins.add(coin6);
-        bagOfCoins.add(coin7);
-
+        bagOfCoins.add(coin10);
+        bagOfCoins.add(coin20);
+        bagOfCoins.add(coin50);
+        bagOfCoins.add(coin10);
+        
+        bagOfCoins.contains(coin5);
         findCoin(bagOfCoins, coin10);
         findCoin(bagOfCoins, coin100);
         displayBagContents(bagOfCoins);
